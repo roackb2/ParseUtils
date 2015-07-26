542e02ac14958984042c825a5d428d6bafd6afce
Jay Liang <roackb2@gmail.com>
2015-07-26 14:52:53 +0800

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,344 @@
+'use strict';
+
+exports.every = function(arr, satisfy) {
+    var value = true;
+    for (var i = 0; i < arr.length; i++) {
+        if (!satisfy(arr[i])) {
+            value = false;
+        }
+    }
+    return value;
+}
+
+exports.some = function(arr, satisfy) {
+    var value = false;
+    for (var i = 0; i < arr.length; i++) {
+        if (satisfy(arr[i])) {
+            value = true;
+        }
+    }
+    return value;
+}
+
+exports.shuffle = function(array) {
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
+
+    while (0 != currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+exports.uuid = function() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    var uuid = s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4()
+    return uuid;
+}
+
+
+exports.rollDice = function(possibility) {
+    var random = Math.floor(Math.random() * 1000);
+    var limit = possibility * 1000;
+    return random < limit;
+}
+
+
+
+exports.WaterFall = function() {
+    this.functions = [];
+    this.params = [];
+    this.counter = 0;
+    this.results = [];
+    var owner = this;
+
+    this.push = function(toDo, param) {
+        owner.params.push(param);
+        owner.functions.push(function() {
+            return toDo.apply(toDo, param).then(function(result) {
+                owner.results.push(result);
+                owner.counter++;
+                if (owner.counter == owner.functions.length) {
+                    return owner.results;
+                } else {
+                    return owner.functions[owner.counter](owner.params[owner.counter]);
+                }
+            }, function(error) {
+                return Parse.Promise.error(error);
+            });
+        });
+    }
+
+    this.start = function() {
+        if (owner.functions.length == 0) {
+            return Parse.Promise.error("no function to do");
+        } else {
+            return owner.functions[0]();
+        }
+    }
+
+}
+
+/*
+logic: 
+
+var params = [...];
+var result = task.apply(this, params);
+while(satisfy(result))  {
+    result = task.apply(this, params);
+    postTask(params);
+}
+*/
+exports.AsyncWhile = function(task, satisfy, postTask, autoRetry) {
+    this.task = task;
+    this.postTask = postTask;
+    this.results = [];
+    this.satisfy = satisfy;
+    var owner = this;
+
+    this.start = function(params) {
+
+        owner.params = params;
+
+        return owner.task.apply(this, params).then(function(result) {
+            owner.results.push(result);
+            if (owner.postTask != undefined) {
+                owner.postTask.apply(this, params);
+            }
+            if (owner.satisfy(result)) {
+                return owner.start(params);
+            } else {
+                return Parse.Promise.as(owner.results);
+            }
+        }, function(err) {
+            if(autoRetry) {
+                console.log("error: ")
+                console.log(err);
+                console.log("auto retry");
+                return owner.start(params);
+            } else {
+                return Parse.Promise.error(err);                
+            }
+        })
+    }
+}
+
+exports.query = function(clss, callback) {
+    var quantum = 1000;
+    var pipeSize = 10;
+    var list = new Array();
+    var finalPromise = new Parse.Promise();
+
+    var countQuery = new Parse.Query(clss);
+    if (typeof(callback) != "undefined") {
+        var newQuery = callback(countQuery);
+        if (newQuery === undefined) {
+            callback(countQuery);
+        } else {
+            countQuery = newQuery;
+        }
+    }
+    countQuery.count().then(function(count) {
+
+        console.log("count: " + count);
+
+        var pages = Math.ceil(count / quantum);
+        var pipes = Math.ceil(pages / pipeSize);
+        var pipeChain = new Array();
+
+        var counter = 0;
+
+        for (var i = 0; i < pipes; i++) {
+            pipeChain[i] = function(start) {
+                console.log("query on " + clss + ", No." + counter + " pipe starts");
+                var pagePromises = new Array();
+                var limit;
+                if (counter == pipes - 1 && pages != pipeSize) {
+                    limit = pages % pipeSize;
+                } else {
+                    limit = pipeSize;
+                }
+                for (var j = 0; j < limit; j++) {
+                    var smallQuery = new Parse.Query(clss);
+                    if (typeof(callback) != "undefined") {
+                        var newQuery = callback(smallQuery);
+                        if (newQuery === undefined) {
+                            callback(smallQuery);
+                        } else {
+                            smallQuery = newQuery;
+                        }
+                    }
+
+                    var date;
+                    if (smallQuery._where && smallQuery._where.createdAt && smallQuery._where.createdAt.$lt) {
+                        date = new Date(smallQuery._where.createdAt.$lt.iso);
+                    }
+                    if (date && date < start) {
+                        smallQuery.lessThan("createdAt", date);
+                    } else {
+                        smallQuery.lessThan("createdAt", start);
+                    }
+                    smallQuery.descending("createdAt");
+                    smallQuery.limit(quantum);
+                    smallQuery.skip(j * quantum);
+                    pagePromises.push(smallQuery.find());
+                }
+                Parse.Promise.when(pagePromises).then(function() {
+                    for (var k = 0; k < arguments.length; k++) {
+                        //console.log("arguments " + k + " length: " + arguments[k].length);
+                        list = list.concat(arguments[k]);
+                    }
+                    console.log("query on " + clss + ", No." + counter + " pipe ends, total list length: " + list.length);
+                    counter++;
+                    if (counter == pipes) {
+                        finalPromise.resolve(list);
+                    } else {
+                        //console.log("createdAt of last list item: " + list[list.length - 1].createdAt);
+                        pipeChain[counter](list[list.length - 1].createdAt);
+                    }
+                }, function(error) {
+                    finalPromise.reject(error);
+                });
+            }
+        }
+        if (pipes == 0) {
+            console.log("query on " + clss + ", return empty array");
+            finalPromise.resolve(new Array());
+        } else {
+            pipeChain[counter](new Date());
+        }
+    }, function(error) {
+        finalPromise.reject(error)
+    });
+
+    return finalPromise;
+
+}
+
+exports.encodeUTF8 = function(s) {
+    return unescape(encodeURIComponent(s));
+}
+
+exports.decodeUTF8 = function(s) {
+    return decodeURIComponent(escape(s));
+}
+
+exports.print = function(obj, ignoreFunc, printFuncContent) {
+    console.log(stringify(obj, ignoreFunc, printFuncContent));
+}
+
+function stringify(obj, ignoreFunc, printFuncContent, former, depth, path) {
+    var indent = "    ";
+    if (ignoreFunc == undefined) {
+        ignoreFunc = false;
+    }
+    if (printFuncContent == undefined) {
+        printFuncContent = false;
+    }    
+    if (former == undefined) {
+        former = "";
+    }
+    if (depth == undefined) {
+        depth = 0;
+    }
+    if (path == undefined) {
+        path = [];
+    }
+
+    var prefix = "";
+    for (var i = 0; i < depth; i++) {
+        prefix += indent;
+    }
+
+    path.push(obj);
+    var encountered = false;
+    for(var i = 0; i < path.length - 1; i++) {
+        if(path[i] === obj) {
+            encountered = true;
+        }
+    }
+
+    var result = former;
+    if (Array.isArray(obj)) {
+        if (!encountered) {
+            if(obj.length > 0) {
+                result += "[\n";
+                for (var i = 0; i < obj.length; i++) {
+                    result += indent + prefix + i + ": ";
+                    result += stringify(obj[i], ignoreFunc, printFuncContent, "", depth + 1, [].concat(path));
+                    if (i < obj.length - 1) {
+                        result += ",\n";
+                    } else {
+                        result += "\n";
+                    }
+                }
+                result += prefix + "]";                 
+            } else {
+                result += "[ ]";
+            }
+        } else {
+            result += "[Circular]";
+        }
+    } else if (typeof(obj) == "object") {
+        if (!encountered) {
+            var count = 0;
+            var totalCount = 0;
+            for (var key in obj) {
+                totalCount++;
+                if (typeof(obj[key]) != "function" || !ignoreFunc) {
+                    count++;
+                }
+            }
+            if(totalCount > 0) {
+                result += "{\n";                
+                var counter = 0;
+                for (var key in obj) {
+                    var value = obj[key];
+                    if (typeof(value) != "function" || !ignoreFunc) {
+                        counter++;
+                        result += prefix + indent + key + ": " + stringify(value, ignoreFunc, printFuncContent, "", depth + 1, [].concat(path));
+                        if (counter < count) {
+                            result += ",\n";
+                        } else {
+                            result += "\n";
+                        }
+                    }
+                }
+                result += prefix + "}"                
+            } else {
+                result += "{}";
+            }
+
+        } else {
+            result += "[Circular]"
+        }
+    } else if (typeof(obj) == "function") {
+        if (printFuncContent) {
+            result += obj;
+        } else {
+            result += "_function_";
+        }
+    } else {
+        result += obj + "";
+    }
+    return result;
+}
+
+
+
 /*
  * Usage: require this file, ex.
  *   var utils = require("utils.js");
